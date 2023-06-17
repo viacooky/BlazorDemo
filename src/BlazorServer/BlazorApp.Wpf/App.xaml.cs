cac2c9dc3b39f6f503c9473b76e92f0e5e20286f
viacooky <viacooky@qq.com>
2023-06-17 17:06:21 +0800

    feat: 初版多端启动

---
+++ src/BlazorServer/BlazorApp.Wpf/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BlazorApp.Wpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        ServerManager.RunServer(Array.Empty<string>());
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        ServerManager.StopServer();
+        base.OnExit(e);
+    }
+}
+
+public class ServerManager
+{
+    private static IHost _host;
+
+    public static void RunServer(string[] args)
+    {
+        _host = CreateHostBuilder(args).Build();
+        _host.Start();
+    }
+
+    public static void StopServer()
+    {
+        _host.Dispose();
+        _host = null;
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+                   
+                   .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                        webBuilder.UseUrls("http://*:5000", "https://*:5001");
+                    });
+    }
+}
---
+++ src/BlazorServer/BlazorApp.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Windows;
+
+namespace BlazorApp.Wpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        webview.Source = new Uri("https://127.0.0.1:5001");
+        webview.CoreWebView2InitializationCompleted += (a, c) =>
+        {
+            webview.CoreWebView2.ContextMenuRequested += (sender, args) =>
+            {
+                var newItem = webview.CoreWebView2.Environment.CreateContextMenuItem(
+                    "在浏览器中打开", null, CoreWebView2ContextMenuItemKind.Command);
+
+                newItem.CustomItemSelected += (send, ex) =>
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", args.ContextMenuTarget.PageUri);
+                };
+
+                args.MenuItems.Insert(args.MenuItems.Count, newItem);
+            };
+        };
+    }
+}
